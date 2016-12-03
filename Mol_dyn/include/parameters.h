4e3cf8d381c44e1fee887397f6b19dfcbfcdf0e3
Andrey1994 <a1994ndrey@gmail.com>
2016-12-03 14:16:48 +0300

    MD: ;ittle fix

--- Mol_dyn/include/parameters.h
+++ Mol_dyn/include/parameters.h
@@ -1,8 +1,8 @@
 #define rc 3
-#define box_size 16
-#define half_box 8
-#define size 512
+#define box_size 10
+#define half_box 5
+#define particles_count 64
 #define total_it 5000
-#define dt 0.0005
-#define initial_dist_by_one_axis 1.7
+#define dt 0.0002
+#define initial_dist_by_one_axis 1.5
 #define initial_dist_to_edge 2
