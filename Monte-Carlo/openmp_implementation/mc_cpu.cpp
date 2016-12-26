6288f1933bac8c449a100a6220795b623df1b47d
Andrey1994 <a1994ndrey@gmail.com>
2016-12-26 23:43:52 +0300

    MC: add Coulomb potentioan OpenMP

--- Monte-Carlo/openmp_implementation/mc_cpu.cpp
+++ Monte-Carlo/openmp_implementation/mc_cpu.cpp
@@ -9,6 +9,7 @@
 #include "parameters.h"
 
 #define NUM_THREADS 8
+#define COULOMB "--coulomb"
 
 struct dim {
     double x;
@@ -18,25 +19,36 @@ struct dim {
 typedef struct dim dim;
 
 void nearest_image(dim *position_arr, dim *nearest);
-void set_initial_state(dim *position_arr);
-void mc_method(dim *position_arr, dim *nearest);
-double calculate_energy_lj(dim *position_arr, dim *nearest);
-double fast_pow(double a, int n);
+void init_problem(dim *position_arr, int *charge);
+void mc_method(dim *position_arr, dim *nearest, int *charge);
+double calculate_energy_lj(dim *position_arr, dim *nearest, int *charge);
+double calculate_energy_coulomb(dim *position_arr, dim *nearest, int *charge);
 
 double max_deviation = 0.01;
+double (*calculate_energy)(dim*, dim*, int*);
 
-int main()
+int main(int argc, char *argv[])
 {
+    calculate_energy = calculate_energy_lj;
+    if (argc > 1){
+        if (!strcmp(argv[1], COULOMB)){
+            calculate_energy = calculate_energy_coulomb;
+        }
+    }
     struct timeb start_total_time;
     ftime(&start_total_time);
     time_t t;
     srand((unsigned)time(&t));
     dim *position_arr = (dim*)malloc(sizeof(dim) * particles_count);
     dim *nearest = (dim*)malloc(sizeof(dim) * particles_count);
-    set_initial_state(position_arr);
-    mc_method(position_arr, nearest);
+    int *charge = (int*)malloc(sizeof(int) * particles_count);
+
+    init_problem(position_arr, charge);
+    mc_method(position_arr, nearest, charge);
+
     free(position_arr);
     free(nearest);
+    free(charge);
     struct timeb end_total_time;
     ftime(&end_total_time);
     printf("Total execution time in ms =  %d", (int)((end_total_time.time - start_total_time.time) * 1000 + end_total_time.millitm - start_total_time.millitm));
@@ -45,7 +57,7 @@ int main()
 
 /////// HELPER FUNCTIONS ///////
 
-void set_initial_state(dim *position_arr) {
+void init_problem(dim *position_arr, int *charge) {
     int count = 0;
     for (double i = -(box_size - initial_dist_to_edge)/2; i < (box_size - initial_dist_to_edge)/2; i += initial_dist_by_one_axis) {
         for (double j = -(box_size - initial_dist_to_edge)/2; j < (box_size - initial_dist_to_edge)/2; j += initial_dist_by_one_axis) {
@@ -54,13 +66,19 @@ void set_initial_state(dim *position_arr) {
                     return;
                 }
                 position_arr[count] = { i,j,l };
+                if (calculate_energy == calculate_energy_coulomb){
+                    if (count & 1)
+                        charge[count] = 1;
+                    else
+                        charge[count] = -1;
+                }
                 count++;
             }
         }
     }
     if( count < particles_count ){
-    	printf("error decrease initial_dist parameter, count is %ld  particles_count is %ld \n", count, particles_count);
-    	exit(1);
+        printf("error decrease initial_dist parameter, count is %ld  particles_count is %ld \n", count, particles_count);
+        exit(1);
     }
 }
 
@@ -89,12 +107,12 @@ void nearest_image(dim *position_arr, dim *nearest){
     }
 }
 
-double calculate_energy_lj(dim *position_arr, dim *nearest){
+double calculate_energy_lj(dim *position_arr, dim *nearest, int *charge){
     nearest_image(position_arr, nearest);
     double energy = 0;
     #pragma omp parallel for reduction(+:energy) num_threads(NUM_THREADS)
     for (int i = 0; i < particles_count; i++) {
-        for (int j = 0; j < particles_count; j++) {
+        for (int j = 0; j < i; j++) {
             float x = nearest[j].x - nearest[i].x;
             float y = nearest[j].y - nearest[i].y;
             float z = nearest[j].z - nearest[i].z;
@@ -117,25 +135,58 @@ double calculate_energy_lj(dim *position_arr, dim *nearest){
                     z += box_size;
             }
             float sq_dist = x * x + y * y + z * z;
-            if ((sq_dist < rc * rc) && (j != i)) {
+            if (sq_dist < rc * rc) {
                 double r6 = sq_dist * sq_dist * sq_dist;
                 double r12 = r6 * r6;
                 energy += 4 * (1 / r12 - 1 / r6);
             }
         }
     }
-    return energy / 2;
+    return energy;
 }
 
-void mc_method(dim *position_arr, dim *nearest) {
+double calculate_energy_coulomb(dim *position_arr, dim *nearest, int *charge){
+    nearest_image(position_arr, nearest);
+    double energy = 0;
+    #pragma omp parallel for reduction(+:energy) num_threads(NUM_THREADS)
+    for (int i = 0; i < particles_count; i++) {
+        for (int j = 0; j < i; j++) {
+            float x = nearest[j].x - nearest[i].x;
+            float y = nearest[j].y - nearest[i].y;
+            float z = nearest[j].z - nearest[i].z;
+            if (x > half_box)
+                x -= box_size;
+            else {
+                if (x < -half_box)
+                    x += box_size;
+            }
+            if (y > half_box)
+                y -= box_size;
+            else {
+                if (y < -half_box)
+                    y += box_size;
+            }
+            if (z > half_box)
+                z -= box_size;
+            else {
+                if (z < -half_box)
+                    z += box_size;
+            }
+            energy += charge[i] * charge[j] / sqrt(x * x + y * y + z * z);
+        }
+    }
+    return energy;
+}
+
+void mc_method(dim *position_arr, dim *nearest, int *charge) {
     double *energy_ar = (double*)malloc(sizeof(double) * nmax);
     register int i = 0;
     register int good_iter = 0;
     int good_iter_hung = 0;
-    double u1 = calculate_energy_lj(position_arr, nearest);
+    double u1 = calculate_energy(position_arr, nearest, charge);
     while (1) {
         if ((good_iter == nmax) || (i == total_it)) {
-            printf("energy is %f \ngood iters percent %f \n", energy_ar[good_iter-1]/particles_count, (float)good_iter/(float)total_it);
+            printf("energy is %f \ngood iters percent %f \n", energy_ar[good_iter-1]/particles_count, (float)good_iter/(float)i);
             break;
         }
         dim *tmp = (dim*)malloc(sizeof(dim)*particles_count);
@@ -149,7 +200,7 @@ void mc_method(dim *position_arr, dim *nearest) {
             tmp[particle].y = tmp[particle].y + ex;
             tmp[particle].z = tmp[particle].z + ex;
         }
-        double u2 = calculate_energy_lj(tmp, nearest);
+        double u2 = calculate_energy(tmp, nearest, charge);
         double deltaU_div_T = (u1 - u2) / Temperature;
         double probability = exp(deltaU_div_T);
         double rand_0_1 = (double)rand() / (double)RAND_MAX;
@@ -162,16 +213,5 @@ void mc_method(dim *position_arr, dim *nearest) {
         }
         i++;
         free(tmp);
-    }
-}
-
-inline double fast_pow(double a, int n) {
-    if (n == 0)
-        return 1;
-    if (n % 2 == 1)
-        return fast_pow(a, n - 1) * a;
-    else {
-        double b = fast_pow(a, n / 2);
-        return b * b;
     }
 }
